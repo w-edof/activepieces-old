beaa882076e60e3c2d6736ce6a6311759cda232d
obenazouz-wedof <144689457+obenazouz-wedof@users.noreply.github.com>
2024-03-08 12:33:45 +0000

    Pending changes exported from your codespace

---
+++ packages/pieces/community/wedof/src/index.ts
@@ -0,0 +1,92 @@
+import {
+  createPiece,
+  PieceAuth,
+  Property,
+} from '@activepieces/pieces-framework';
+import { PieceCategory } from '@activepieces/shared';
+import { newRegistrationFolderCreated } from './lib/triggers/new-registration-folder-created';
+import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+import { wedofCommon } from './lib/common/wedof';
+
+/*
+export const wedofAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: 'Please enter your API Key gived by wedof',
+  validate: async ({ auth }) => {
+    const response = await httpClient.sendRequest({
+      method: HttpMethod.GET,
+      url: wedofCommon.fakeBaseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': auth as string,
+        Host: wedofCommon.host,
+      },
+    });
+    if (response.status == 200) {
+      return {
+        valid: true,
+      };
+    } else {
+      return {
+        valid: false,
+        error: 'Invalid Api Key',
+      };
+    }
+  },
+});
+*/
+ export const wedofAuth = PieceAuth.CustomAuth({
+  required: true,
+  props: {
+    id: Property.ShortText({
+      displayName: 'API Key',
+      required: false,
+      description: 'Please enter your API Key gived by wedof',
+      defaultValue: 'rrrrr'
+    }),
+    apiKey: Property.ShortText({
+      displayName: 'API Key',
+      required: true,
+      description: 'Please enter your API Key gived by wedof',
+    }),
+  },
+  validate: async ({ auth }) => {
+   /* const response = await httpClient.sendRequest({
+      method: HttpMethod.GET,
+      url: wedofCommon.fakeBaseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': auth.apiKey,
+        Host: wedofCommon.host,
+      },
+    });*/
+    if (auth.apiKey==="test") {
+       auth.id = "test";
+      return {
+        valid: true,
+      }; 
+    } else {
+      auth.id = "noooo";
+      return {
+        valid: false,
+        error: 'Invalid Api Key',
+      };
+    }
+  },
+});
+
+export const wedof = createPiece({
+  displayName: 'Wedof',
+  auth: wedofAuth,
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://www.wedof.fr/api/doc/logo.png',
+  categories: [
+    PieceCategory.SALES_AND_CRM,
+    PieceCategory.CONTENT_AND_FILES,
+    PieceCategory.PRODUCTIVITY,
+  ],
+  authors: ['Wedof'],
+  actions: [],
+  triggers: [newRegistrationFolderCreated],
+});
